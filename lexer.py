5ed0cdd8947a43eb72bb22c6ef0c4d0c17944467
milder <milderhc@gmail.com>
2016-09-24 16:30:16 -0500

    first commit

---
+++ lexer.py
@@ -0,0 +1,415 @@
+#
+# Lexer Taller 1
+#
+#
+from itertools import chain
+from sys import stdin
+import re
+import filecmp
+
+def initialize_keywords():
+    global keywords
+    keywords = {}
+    keywords["algoritmo"] = "algoritmo"
+    keywords["finalgoritmo"] = "finalgoritmo"
+    keywords["proceso"] = "proceso"
+    keywords["finproceso"] = "finproceso"
+    keywords["definir"] = "definir"
+    keywords["como"] = "como"
+    keywords["numero"] = "numero"
+    keywords["numerico"] = "numerico"
+    keywords["entero"] = "entero"
+    keywords["caracter"] = "caracter"
+    keywords["real"] = "real"
+    keywords["logico"] = "logico"
+    keywords["texto"] = "texto"
+    keywords["cadena"] = "cadena"
+    keywords["verdadero"] = "verdadero"
+    keywords["falso"] = "falso"
+    keywords["leer"] = "leer"
+    keywords["escribir"] = "escribir"
+    keywords["dimension"] = "dimension"
+    keywords["para"] = "para"
+    keywords["hasta"] = "hasta"
+    keywords["con"] = "con"
+    keywords["paso"] = "paso"
+    keywords["hacer"] = "hacer"
+    keywords["finpara"] = "finpara"
+    keywords["borrar"] = "borrar"
+    keywords["pantalla"] = "pantalla"
+    keywords["esperar"] = "esperar"
+    keywords["tecla"] = "tecla"
+    keywords["segundos"] = "segundos"
+    keywords["milisegundos"] = "milisegundos"
+    keywords["si"] = "si"
+    keywords["entonces"] = "entonces"
+    keywords["sino"] = "sino"
+    keywords["finsi"] = "finsi"
+    keywords["segun"] = "segun"
+    keywords["de"] = "de"
+    keywords["otro"] = "otro"
+    keywords["modo"] = "modo"
+    keywords["finsegun"] = "finsegun"
+    keywords["mientras"] = "mientras"
+    keywords["finmientras"] = "finmientras"
+    keywords["repetir"] = "repetir"
+    keywords["hasta"] = "hasta"
+    keywords["que"] = "que"
+    keywords["subproceso"] = "subproceso"
+    keywords["finsubproceso"] = "finsubproceso"
+
+    #Operators as keywords
+    keywords["mod"] = "mod"
+    keywords["no"] = "no"
+
+
+def initialize_operators():
+    global operators
+    operators = {}
+    operators["~"] = "token_neg"
+    operators["="] = "token_igual"
+    operators["<-"] = "token_asig"
+    operators["<>"] = "token_dif"
+    operators["<"] = "token_menor"
+    operators[">"] = "token_mayor"
+    operators["<="] = "token_menor_igual"
+    operators[">="] = "token_mayor_igual"
+    operators["+"] = "token_mas"
+    operators["-"] = "token_menos"
+    operators["/"] = "token_div"
+    operators["*"] = "token_mul"
+    operators["%"] = "token_mod"
+    operators[";"] = "token_pyc"
+    operators[":"] = "token_dosp"
+    operators["("] = "token_par_izq"
+    operators[")"] = "token_par_der"
+    operators["["] = "token_cor_izq"
+    operators["]"] = "token_cor_der"
+    operators["|"] = "token_o"
+    operators["&"] = "token_y"
+    operators["o"] = "token_o"
+    operators[","] = "token_coma"
+    operators["^"] = "token_pot"
+
+    operators["y"] = "token_y"
+    operators["no"] = "token_neg"
+    operators["mod"] = "token_mod"
+
+
+def is_other(character):
+    return re.match("[ \t\n]", character)
+
+
+def is_digit(character):
+    return re.match("[0-9]", character)
+
+
+def is_letter(character):
+    return re.match("[a-zA-Z]", character)
+
+def is_operator(character):
+    global operators
+
+    return character in operators
+
+
+def is_string(character):
+    return character == "\"" or character == "'"
+
+
+def lex_error():
+    global row, col
+
+    return ">>> Error lexico (linea: " + str(row + 1) + ", posicion: " + str(col + 1) + ")"
+
+
+def read_comment(line):
+    global row, col
+
+    length_line = len(line)
+
+    token = None
+    advance = 0
+
+    index = col
+    if index + 1 < length_line and line[index + 1] == "/": # It's a comment
+        token = line[index:-1]
+        advance = len(token)
+
+    return token, advance
+
+
+def read_number(line):
+    global row, col
+
+    length_line = len(line)
+
+    index = col
+
+    token = None
+    advance = 0
+
+    while index < length_line:
+        character = line[index]
+
+        if is_digit(character) or character == ".":
+            index += 1
+        elif is_operator(character) or is_other(character): # Read integer number
+            break
+        else:
+            return None, 0  # Lexer error
+
+    token = line[col:index]
+    advance = len(token)
+    return token, advance
+
+def read_identifier(line):
+    global row, col
+
+    length_line = len(line)
+
+    token = ""
+    advance = 0
+
+    index = col
+
+    while index < length_line:
+        character = line[index]
+
+        if is_letter(character) or is_digit(character) or character == "_":
+            index += 1
+            token += character
+        else:
+            break
+
+    if token is not None:
+        advance = len(token)
+
+    return token, advance
+
+
+def read_keyword_identifier(line):
+    global row, col, keywords
+
+    length_line = len(line)
+
+    index = col
+
+    token = None
+    advance = 0
+    type = None
+
+    while index < length_line:
+        character = line[index]
+
+        if not is_letter(character):
+            if is_digit(character) or character == "_":
+                token, advance = read_identifier(line)
+                type = "id"
+            else:
+                value = line[col:index:].lower()
+                if value in keywords:
+                    token = keywords[value]
+                    advance = len(token)
+                    type = "token_keyword"
+                else:
+                    token, advance = read_identifier(line)
+                    type = "id"
+            break
+        else:
+            index += 1
+
+    if index == length_line:
+        value = line[col:index:].lower()
+        if value in keywords:
+            token = keywords[value]
+            advance = len(token)
+            type = "token_keyword"
+        else:
+            token, advance = read_identifier(line)
+            type = "id"
+
+    return type, token, advance
+
+
+def read_operator(line):
+    global row, col
+
+    length_line = len(line)
+
+    index = col
+
+    token = None
+    advance = 0
+
+    if index + 1 < length_line and is_operator(line[index:index + 2: 1]):
+        token = line[index:index + 2:]
+        advance = 2
+    else:
+        token = line[index]
+        advance = 1
+
+    return token, advance
+
+
+def read_string(line):
+    global row, col
+    length_line = len(line)
+
+    advance = 0
+    token = None
+    index = col + 1 # Skip quote or apostrophe
+
+    while index < length_line:
+        if line[index] == "\"" or line[index] == "'":
+            token = line[col + 1:index]
+            return token, len(token) + 2
+
+        index += 1
+
+    return token, advance
+
+def next_token(line):
+    global col, operators
+
+    character = line[col]
+
+    type = None
+    token = None
+    advance = 0
+
+    # End line there is not token
+    if col == len(line):
+        token = ""
+        advance = 0
+        type = "fin_linea"
+
+    # Space, tab or end line
+    if token is None and is_other(character):
+        token = character
+        advance = 1
+        type = "token_otro"
+
+    # Could be a comment
+    if token is None and character == "/":
+        token, advance = read_comment(line)
+        if token is not None:
+            type = "token_comentario"
+
+    # Integer or real number
+    if token is None and is_digit(character):
+        token, advance = read_number(line)
+        if token is not None:
+            #Not sure about this (pseint read it right)
+            if token[-1] == ".":
+                type = "lex_error"
+            else:
+                type = "token_real" if "." in token else "token_entero"
+
+    # Operator
+    if token is None and is_operator(character):
+        token, advance = read_operator(line)
+        if token is not None:
+            type = operators[token]
+
+    # Keyword or identifier
+    if token is None and is_letter(character):
+        type, token, advance = read_keyword_identifier(line)
+
+    # String
+    if token is None and is_string(character):
+        token, advance = read_string(line)
+        if token is not None:
+            type = "token_cadena"
+
+    if token is None:
+        type = "lex_error"
+        return type, token, row, col
+    else:
+        index = col
+        col += advance
+        return type, token, row, index
+
+
+def build_token(title, lexema, r, c):
+    global keywords, operators
+
+    token = "<"
+
+    if lexema in keywords:
+        if lexema not in operators:
+            token += lexema
+        else:
+            token += operators[lexema]
+    elif lexema in operators:
+        token += title
+    else:
+        token += title + "," + lexema.lower()
+
+    token += "," + str(r) + "," + str(c) + ">"
+
+    return token
+
+
+def read_data():
+    global tokens, row, col
+
+    lines = stdin.readlines()
+
+    tokens = []
+
+    type = ""
+
+    for line in lines:
+        while col < len(line) and type is not "lex_error":
+            type, token, x, y = next_token(line)
+            if type is not "token_otro" and type is not "lex_error" and type is not "token_comentario":
+                #print build_token(type, token, x + 1, y + 1)
+
+                #Prints in file
+                stdout.write(build_token(type, token, x + 1, y + 1) + "\n")
+
+        if type is "lex_error":
+            #print lex_error()
+
+            # Prints in file
+            stdout.write(str(lex_error()) + "\n")
+            break
+
+        row += 1
+        col = 0
+
+    #return tokens
+
+tokens = []
+keywords = {}
+operators = {}
+
+initialize_keywords()
+initialize_operators()
+row = 0
+col = 0
+
+examples = 12
+for i in range(12,examples + 1):
+    stdin = open("ejemplos/" + str(i) + ".in", "r")
+    output_file = str(i) + ".out"
+    stdout = open("output/" + output_file, "w")
+
+    row = 0
+    col = 0
+    read_data()
+
+    stdout.close()
+
+    correct = ""
+    with open("ejemplos/" + output_file) as f:
+        correct.join(line.strip() for line in f)
+
+    output = ""
+    with open("output/" + output_file) as f:
+        output.join(line.strip() for line in f)
+
+    assert correct == output, "Error in file " + output_file
+
+
